298069d0293e26a9e5ad968b75dda85c263cb8b5
Milad Heidari <milad.h9999@gmail.com>
2024-11-30 10:22:49 -0800

    Add: one more hidden layer in observation model, Add: transition model (prior)

--- controller/models.py
+++ controller/models.py
@@ -5,7 +5,8 @@ from torch.distributions import Normal
 
 class ObservationModel(nn.Module):
     """
-        outputs mean for p(o_t | s_t) ~ N(f(s_t), I)
+        p(o_t | s_t)
+        constant variance
     """
     def __init__(
         self,
@@ -18,8 +19,41 @@ class ObservationModel(nn.Module):
         self.mlp_layers = nn.Sequential(
             nn.Linear(state_dim, hidden_dim),
             nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, observation_dim),
         )
 
     def forward(self, state):
         return self.mlp_layers(state)
+    
+
+class TransitionModel(nn.Module):
+    """
+        p(s_t | s_{t-1}, a_{t-1})
+    """
+    def __init__(
+        self,
+        state_dim: int,
+        action_dim: int,
+        hidden_dim: int,
+    ):
+        super().__init__()
+
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(state_dim + action_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+        )
+
+        self.mean_head = nn.Linear(hidden_dim, state_dim)
+        self.log_std_head = nn.Linear(hidden_dim, state_dim)
+
+    def forward(self, prev_state, prev_action):
+        hidden = self.mlp_layers(
+            torch.cat([prev_state, prev_action], dim=1)
+        )
+        mean = self.mean_head(hidden)
+        log_std = self.log_std_head(hidden)
+        return Normal(mean, log_std.exp())
